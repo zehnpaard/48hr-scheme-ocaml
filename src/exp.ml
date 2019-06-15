ed2a09f0a4b70c2211758da45227c39941ccd7ad
zehnpaard <zehnpaard@googlemail.com>
2019-06-15 22:26:10 +0900

    Add to_string for expressions

--- src/exp.ml
+++ src/exp.ml
@@ -4,3 +4,15 @@ type t = Atom of string
        | Number of int
        | String of string
        | Bool of bool
+
+let rec to_string = function
+  | Atom a -> a
+  | List es ->
+      let s = List.map to_string es |> String.concat " " in
+      Printf.sprintf "(%s)" s
+  | DottedList (es, e) ->
+      let s = List.map to_string es |> String.concat " " in
+      Printf.sprintf "(%s . %s)" s (to_string e)
+  | Number n -> string_of_int n
+  | String s -> "\"" ^ s ^ "\""
+  | Bool b -> string_of_bool b
